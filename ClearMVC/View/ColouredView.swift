93416b81eb0403bb109cedecbb90e27590369226
Peter Leung <ys.leung.uk@gmail.com>
2018-09-09 19:57:30 +0800

    Added a simple view to the project.

---
+++ ClearMVC/View/ColouredView.swift
@@ -0,0 +1,17 @@
+//
+//  ColouredView.swift
+//  ClearMVC
+//
+//  Created by Peter Leung on 9/9/2018.
+//  Copyright © 2018 Peter Leung. All rights reserved.
+//
+
+import UIKit
+
+class ColouredView: UIView {
+
+    override func awakeFromNib() {
+        backgroundColor = .blue
+    }
+
+}
