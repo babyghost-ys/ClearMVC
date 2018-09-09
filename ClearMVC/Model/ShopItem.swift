c08ffb2d91116bb4855c1266681288a2b02268a5
Peter Leung <ys.leung.uk@gmail.com>
2018-09-09 19:53:59 +0800

    Constructed the basic data class model.

--- ClearMVC/Model/ShopItem.swift
+++ ClearMVC/Model/ShopItem.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class ShopItem {
+    private(set) var name: String
+    private(set) var price: Double
+    
+    init(name: String, price: Double) {
+        self.name = name
+        self.price = price
+    }
+}
