a528569ad9cd22bca3932bad77f1ea6062fa7e74
Peter Leung <ys.leung.uk@gmail.com>
2018-09-09 20:06:19 +0800

    Added a label to load the text from the ShopItem data model.

--- ClearMVC/Controller/ViewController.swift
+++ ClearMVC/Controller/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let firstItem = ShopItem(name: "Product A", price: 100.99)
+        mainLabel.textColor = .white
+        mainLabel.text = "You have \(firstItem.name) at $\(firstItem.price)"
     }
 
 }
